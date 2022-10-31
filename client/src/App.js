f9f70112bb3123b88143668bb1e5f70a72459311
Pancham Bopanna D <pancham271195@gmail.com>
2022-10-31 18:13:48 +0530

    FE - Login/Sign up

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./compnents/layout/Navbar";
 import Landing from "./compnents/layout/Landing";
+import Register from "./compnents/auth/Register";
+import Login from "./compnents/auth/Login";
 
 function App() {
   return (
-    <Fragment className="App">
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+        </Routes>
+        <section className="container">
+          <Routes>
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/login" element={<Login />} />
+          </Routes>
+        </section>
+      </div>
+    </BrowserRouter>
   );
 }
 
