95353275de2e9c96fdeaa1359f35924e8ec49359
Pancham Bopanna D <pancham271195@gmail.com>
2022-11-01 12:28:17 +0530

    FE - Alert reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,33 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./compnents/layout/Navbar";
-import Landing from "./compnents/layout/Landing";
-import Register from "./compnents/auth/Register";
-import Login from "./compnents/auth/Login";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Alert from "./components/layout/Alert";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Landing />} />
-        </Routes>
-        <section className="container">
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
           <Routes>
-            <Route exact path="/register" element={<Register />} />
-            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/" element={<Landing />} />
           </Routes>
-        </section>
-      </div>
-    </BrowserRouter>
+          <section className="container">
+            <Alert />
+            <Routes>
+              <Route exact path="/register" element={<Register />} />
+              <Route exact path="/login" element={<Login />} />
+            </Routes>
+          </section>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
